e4246120c6de92272d328f75927193dadb8ef4b4
JTZ <tesucunjanner@gmail.com>
2023-11-18 00:08:52 -0600

    update2 hopitl

--- Hopitl/src/app/app.module.ts
+++ Hopitl/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -10,12 +9,11 @@ import { RdoctoresComponent } from './rdoctores/rdoctores.component';
 import { ExamenesComponent } from './examenes/examenes.component';
 import { MedicamentosComponent } from './medicamentos/medicamentos.component';
 import { RecetaComponent } from './receta/receta.component';
-import { FormsModule } from '@angular/forms';
 
-import { RouterModule, Routes } from '@angular/router';
+
+import { RouterModule,Routes } from '@angular/router';
 
 const routes: Routes = [
-<<<<<<< HEAD
 {
   path:'Login',
   component: LoginComponent
@@ -42,32 +40,6 @@ const routes: Routes = [
 },
 
 
-=======
-  {
-    path: 'Login',
-    component: LoginComponent,
-  },
-  {
-    path: 'Rclientes',
-    component: RclientesComponent,
-  },
-  {
-    path: 'Rdoctores',
-    component: RdoctoresComponent,
-  },
-  {
-    path: 'Examenes',
-    component: ExamenesComponent,
-  },
-  {
-    path: 'Medicamentos',
-    component: MedicamentosComponent,
-  },
-  {
-    path: 'Receta',
-    component: RecetaComponent,
-  },
->>>>>>> 82ffbda50fb3196169bd142fe4f5eaa7c66ef58e
 ];
 
 @NgModule({
@@ -78,23 +50,17 @@ const routes: Routes = [
     RdoctoresComponent,
     ExamenesComponent,
     MedicamentosComponent,
-    RecetaComponent,
+    RecetaComponent
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     FormsModule,
     RouterModule.forRoot(routes)
-=======
-    RouterModule.forRoot(routes),
-    FormsModule,
-    HttpClientModule,
->>>>>>> 82ffbda50fb3196169bd142fe4f5eaa7c66ef58e
   ],
   exports: [
     RdoctoresComponent
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
